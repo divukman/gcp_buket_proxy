cefd9fd9c7ba77e5bb236ef55be9ed4a1a0bf0fa
dimitar <dimitar.vukman@bluedropmedical.com>
2022-09-05 13:31:58 +0200

    WIP

---
+++ bucket-proxy/src/main/java/tech/dimitar/gcp/bucketproxy/BucketProxyApplication.java
@@ -0,0 +1,13 @@
+package tech.dimitar.gcp.bucketproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketProxyApplication.class, args);
+	}
+
+}
---
+++ bucket-proxy/src/test/java/tech/dimitar/gcp/bucketproxy/BucketProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.gcp.bucketproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
